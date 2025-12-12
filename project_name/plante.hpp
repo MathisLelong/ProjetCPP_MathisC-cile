9a80f3754843b3b5b83808df16ee2bea44d2ee08
Lelong Mathis <mlelong@insa-11277>
2025-12-12 09:44:56 +0100

    implémentation de Plante

---
+++ project_name/plante.hpp
@@ -0,0 +1,31 @@
+#ifndef _plante_hpp
+#define _plante_hpp
+
+using namespace std ;
+
+class Plante {
+
+protected :
+
+  static int Compteur
+
+  public :
+
+  Plante();
+
+  ~Plante();
+
+  virtual void Caracteristiques()=0;
+  virtual int TempsDePousse()=0;
+
+
+  int GetCompteur ();
+
+  
+
+};
+
+
+
+
+#endif
