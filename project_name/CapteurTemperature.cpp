0b12b34dad3752b40c1c43096079013ec842245c
Cecile Maurel <cecilemaurel10@gmail.com>
2025-12-18 15:57:58 +0100

    creationRose

---
+++ project_name/CapteurTemperature.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+#include "CapteurTemperature.hpp"
+
+using namespace std ;
+
+CapteurTemperature::CapteurTemperature(){
+
+
+}
+
+
+CapteurTemperature::~CapteurTemperature(){
+
+
+}
+
---
+++ project_name/CapteurTemperature.hpp
@@ -0,0 +1,17 @@
+#ifndef _CapteurTemperature_hpp
+#define _CapteurTemperature_hpp
+
+
+using namespace std;
+
+class CapteurTemperature {
+
+    CapteurTemperature(); 
+
+
+  ~CapteurTemperature(); 
+
+};
+
+
+#endif
